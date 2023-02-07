1485ab10794d1539cb64e2ae576de2e5fd6ee7f3
MatheusVict <matheusvictorhenrique@gmail.com>
2023-02-06 21:01:39 -0300

    users crud

---
+++ src/users/dto/user-create.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class UserCreateDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword({
+    minLength: 8,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserCreateDTO } from './dto/user-create.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() user: UserCreateDTO) {
+    return await this.usersService.createUser(user);
+  }
+
+  @Get()
+  async findAll(@Query('userId') userId: string) {
+    if (!userId) return await this.usersService.getAll();
+
+    return await this.usersService.getOneForId(userId);
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() user: UpdateUserDTO) {
+    await this.usersService.update(id, user);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    await this.usersService.deleteUser(id);
+  }
+}
