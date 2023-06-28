955f0124a84c41a1e21a296563508d4aceeaac47
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-28 16:43:57 -0300

    prune validation

--- src/posts/interfaces/posts.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface PostsInterface {
-  id?: string;
-  title: string;
-  slug: string;
-  content: string;
-  idUser: string;
-  idComentarys?: string[];
-  likes?: any[];
-}
--- src/users/interfaces/users.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface UserInterface {
-  _id?: string;
-  name: string;
-  email: string;
-  password: string;
-  isAdm: number;
-  picture: string;
-}
