902705e2904f5e6111a5cdf4c9106e8e89d3b222
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-28 20:36:47 -0300

    basic user test setup

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Model } from 'mongoose';
+import { User } from './schemas/users.schemas';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userModel: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('users'),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userModel = module.get<Model<User>>(getModelToken('users'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userModel).toBeDefined();
   });
 });
