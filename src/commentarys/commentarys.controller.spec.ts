f596d0d54dda0ee20e565b1bff8f94ceb8465bd9
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-29 13:49:14 -0300

    commentarys basic test setup

--- src/commentarys/commentarys.controller.spec.ts
+++ src/commentarys/commentarys.controller.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentarysController } from './commentarys.controller';
+import { CommentarysService } from './commentarys.service';
 
 describe('CommentarysController', () => {
   let controller: CommentarysController;
+  let service: CommentarysService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentarysController],
+      providers: [
+        {
+          provide: CommentarysService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<CommentarysController>(CommentarysController);
+    service = module.get<CommentarysService>(CommentarysService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
