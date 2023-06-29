ddaf85b7cfd307c2179d237728f426370e444c0b
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-29 12:12:19 -0300

    user controller basic test setup

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let userscontroller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    userscontroller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userscontroller).toBeDefined();
+    expect(usersService).toBeDefined();
   });
 });
