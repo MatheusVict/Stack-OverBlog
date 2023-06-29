88a070b86137c53509548ac5747675398e927442
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-29 13:25:49 -0300

    postService basic test setup

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { Model } from 'mongoose';
+import { Posts } from './schemas/posts.schema';
+import { UsersService } from 'src/users/users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from 'src/users/schemas/users.schemas';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let postsModel: Model<Posts>;
+  let usersService: UsersService;
+  let userModel: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: getModelToken('posts'),
+          useValue: Model,
+        },
+        UsersService,
+        {
+          provide: getModelToken('users'),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    postsModel = module.get<Model<Posts>>(getModelToken('posts'));
+    usersService = module.get<UsersService>(UsersService);
+    userModel = module.get<Model<User>>(getModelToken('users'));
   });
 
   it('should be defined', () => {
